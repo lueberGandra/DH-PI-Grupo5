2f78eb59a3d0a8a8f6916957f15c5282c00c1697
lueberGandra <lueber_emp@hotmail.com>
2021-03-12 16:17:05 -0300

    bugfix nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,27 @@
 import React from 'react'
 import './style.css'
-import {Navbar} from 'react-bootstrap'
+
 
 
 const Header = () => {
   return (
-    <header> 
-         <Navbar className="navbar navbar-expand-lg navbar-dark " collapseOnSelect expand="lg" bg="dark" variant="dark">
-         <div className="container-fluid "> 
-         <a className="navbar-brand" href="#"> <img src="./img/logo.png" alt="logo da empresa"></img></a>
-         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-lg-0">
-              <li className="nav-item" className='bg-primary'>
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Artigos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="#" tabindex="-1" aria-disabled="true">Podcasts</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Videos</a>
-              </li>
-            </ul>
-                <div id="divBuscaHeader">
-                {/*<input className="placeholder-inactive" type="text" id= "textoBusca" placeholder="Pesquisar"> */}
-                <img src='./img/lupa-header.png' href="#" id="bntBusca" alt="Buscar"/>
-                </div>
-                </div>
-         </div>
-         </Navbar>
-    </header>
+    <nav>   
+    <input id="nav-toggle" type="checkbox"/>
+    <div class="logo"><img src="./img/logo.png" alt="logo"/><strong>Site</strong></div>
+    <ul class="links">
+        <li><a href="">Home</a></li>
+        <li><a href="">Artigos</a></li>
+        <li><a href="">Podcasts</a></li>
+        <li><a href="">Videos</a></li>       
+        <li><a href="">Sobre</a></li>       
+    </ul>
+    <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </label>
+</nav>
+    
   )
 }
 
