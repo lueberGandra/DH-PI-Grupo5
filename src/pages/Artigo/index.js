07fcafd094bfa84c11b83c430cf7008b15b3be59
lucasokano <luc4sko@hotmail.com>
2021-03-13 17:04:06 -0300

    estrutura do artigo

---
+++ src/pages/Artigo/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './style.css'
+import {Navbar} from 'react-bootstrap'
+
+
+const Header = () => {
+  return (
+    <div id="artigo"> 
+        <h1>Manchete do Artigo</h1>    
+        <h2>Resumo curto sobre o artigo</h2>
+        <h3 id="nome do jornalista">Jornalista</h3>
+        <date>12 de março de 2021, 16:37</date>
+
+        <div class="conteiner-fluid">
+        <img class="img-responsive"></img>    
+        </div>    
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut rhoncus dui. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sed mi vel neque cursus iaculis. In porttitor pharetra ante ac lacinia. Nulla id dolor in elit gravida faucibus. Nullam auctor dolor in posuere pulvinar. Donec commodo dictum velit, et sodales orci. Aenean eu mollis diam. Quisque in consectetur arcu.</p>
+
+        <p>Vivamus a leo sit amet tellus lobortis fringilla. Nam condimentum erat vitae ex ornare hendrerit. Integer lobortis velit massa, sit amet condimentum risus commodo eu. Fusce iaculis nisl augue, ut pulvinar nisi molestie in. Mauris placerat tristique elit sed sollicitudin. Curabitur non eros in mi dignissim vestibulum vitae in odio. Vestibulum sed tempor purus. Quisque leo lectus, aliquet id purus quis, condimentum rutrum nisi. Mauris vitae eleifend libero. Donec finibus pharetra finibus. Vestibulum faucibus, lacus eget accumsan maximus, ligula ex malesuada augue, nec vestibulum ex leo sit amet erat. Nunc at eleifend enim. Pellentesque convallis elementum volutpat. Quisque commodo mi nec magna lacinia gravida. Vivamus eu erat ac massa condimentum luctus. Sed commodo quam non orci viverra fermentum.</p>
+
+        <p>Nulla rutrum et dolor ac suscipit. Donec fringilla fringilla eros. Mauris pulvinar vulputate vehicula. Sed ex purus, tempus a tortor ac, fermentum imperdiet purus. Suspendisse eros mauris, hendrerit tincidunt euismod ac, blandit porttitor purus. Cras blandit luctus pharetra. Aliquam accumsan volutpat ipsum sit amet egestas. Nunc cursus ligula leo, a imperdiet arcu varius et. Praesent eget aliquet felis. Vivamus mollis augue risus, sed dignissim arcu consectetur at. Nunc sit amet leo ut felis tincidunt lobortis viverra vel neque. Nulla a sapien ac risus molestie interdum. Proin urna nulla, commodo et viverra quis, iaculis in turpis. Sed viverra sem orci, nec mollis tortor placerat ac.</p>
+
+        <p>Donec arcu sapien, semper non est quis, pretium pellentesque nunc. Nam pulvinar, lectus vitae posuere fermentum, dui dolor pretium metus, et euismod sem lectus sit amet sem. Nunc laoreet nec mauris eu bibendum. Donec id ligula a turpis vehicula viverra eget sed risus. Vivamus nulla urna, semper laoreet lectus a, eleifend dignissim elit. In pretium posuere dolor. Sed mattis, arcu ac vestibulum dictum, nibh lorem facilisis nunc, at consequat risus ligula in velit. Nullam id egestas sapien. Sed ultrices consectetur felis ut convallis. Cras dapibus bibendum enim, et fringilla purus condimentum ut. In rhoncus nisi at velit ullamcorper tincidunt. Curabitur quis tellus est. Proin sed ipsum at risus aliquet pulvinar eget ac velit. Duis diam sem, ullamcorper a viverra sit amet, placerat nec dolor. Nullam pharetra purus ac tortor volutpat congue. Mauris vitae lacus tempus, mattis justo at, sollicitudin ipsum.</p>
+
+        <p>Ut orci dolor, egestas eget enim quis, consequat feugiat leo. Fusce ac orci tempus, dignissim nibh nec, lacinia eros. Maecenas sagittis id neque elementum fringilla. Mauris maximus mattis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum fringilla dictum lectus sit amet tristique. Morbi condimentum dui a tortor blandit congue.</p>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Header
