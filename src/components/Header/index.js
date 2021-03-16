edb7c4d4411bda113acd0a003225201dba01c3e4
lueberGandra <lueber_emp@hotmail.com>
2021-03-16 20:55:17 -0300

    bugfix home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,18 +7,18 @@ const Header = () => {
   return (
     <nav>   
     <input id="nav-toggle" type="checkbox"/>
-    <div class="logo"><img src="./img/logo.png" alt="logo"/><strong>Site</strong></div>
-    <ul class="links">
+    <div className="logo"><img src="./img/logo.png" alt="logo"/><strong>Site</strong></div>
+    <ul className="links">
         <li><a href="">Home</a></li>
         <li><a href="">Artigos</a></li>
         <li><a href="">Podcasts</a></li>
         <li><a href="">Videos</a></li>       
         <li><a href="">Sobre</a></li>       
     </ul>
-    <label for="nav-toggle" class="icon-burger">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
+    <label for="nav-toggle" className="icon-burger">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
     </label>
 </nav>
     
